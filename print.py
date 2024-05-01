beb178ca71c01462be8469d266c2ee84d8c40955
rudy deceliere <61892880+deceliere@users.noreply.github.com>
2024-05-01 08:40:05 +0200

    added: print header / footer, print elapsed time for printing, corr incorrect offset on jump method, add duration

--- print.py
+++ print.py
@@ -4,20 +4,23 @@ import random
 import ewave
 import sys
 import time
+import tempfile
 
 import matplotlib.pyplot as plt
 # from matplotlib.animation import FuncAnimation
 
 # soundfile = "HIROSHIMA30_1.wav"
 soundfile = "iceland(dematrix).wav"
-sampleStep = 10
-offset = 2000 * sampleStep
+sampleStep = 1000
+offset = 3000 * sampleStep
 previous_level = 0
 draw_line = True # if false, draw dots
-printOrNot = False # False if test mode, without printer attached
-lineWidth = 2 # printed line width
+printOrNot = True # False if test mode, without printer attached
+showImage = False
+lineWidth = 3 # printed line width
 getAverage = False # False means pickup raw sample value each sampleStep
 currentDot = 0
+totalSample = 0
 
 # Vérifier le nombre d'arguments
 if len(sys.argv) < 2:
@@ -42,7 +45,9 @@ def generer_image(largeur, hauteur, taille_carre, position, current_image):
     print("current level= ", current_level)
     draw = ImageDraw.Draw(image)
     if draw_line:
-        draw.line([previous_level, hauteur, current_level, 0], fill="black", width=lineWidth)
+        draw.line([previous_level, 0, current_level, hauteur], fill="black", width=lineWidth)
+        # draw.line([0, hauteur, 100, 0], fill="black", width=lineWidth)
+        # draw.line([100, hauteur, 0, 0], fill="black", width=lineWidth)
     else:
         # Dessin d'un carré noir à la position transmise par la fonction
         x = position
@@ -54,9 +59,11 @@ def generer_image(largeur, hauteur, taille_carre, position, current_image):
     # return image
 
 def get_sample(file):
+    global totalSample
     with ewave.open(file) as w:
         print("samplerate = {0.sampling_rate} Hz, length = {0.nframes} samples, "
               "channels = {0.nchannels}, dtype = {0.dtype!r}".format(w))
+        totalSample = w.nframes
         data = w.read()
         return data
     
@@ -65,6 +72,7 @@ def scale(valeur, valeur_min, valeur_max, echelle_min, echelle_max):
     valeur_echelle = (valeur - valeur_min) * (echelle_max - echelle_min) / (valeur_max - valeur_min) + echelle_min
     return int(valeur_echelle)
 
+# average method
 def average(data, location, q):
     #moyenne de q points
     average = 0
@@ -74,6 +82,19 @@ def average(data, location, q):
         # print("average++ =", average)
     average /= q
     return int(average)
+
+# which method used
+def getMethod():
+     if getAverage:
+        return("average")
+     else:
+        return("jump")
+
+# total printing duration elapsed
+def getPrintDuration():
+    totalPrintDur = printFinishTime - printStartTime
+    m, s = divmod(totalPrintDur, 60)
+    return ("{}m{}s".format(int(m), int(s)))
 
 def show_image():
      # Afficher l'image
@@ -104,16 +125,42 @@ if getAverage:
         print("methode= Average\n")
 else:
         print("methode= Pick sample\n")
+        
+totalDurationM, totalDurationS = divmod(totalSample / 48000, 60)
+header = "sampleStep={}\ndots/lines to print={}\noffset={}\nmethod={}\nprint length(S)={}\ntotal length={}m{}s\n\n\n".format(sampleStep, dots, offset, getMethod(), round(sampleStep * dots / 48000, 2), int(totalDurationM), int(totalDurationS))
+
+with tempfile.NamedTemporaryFile(mode='w', delete=False) as fichier_temp:
+    fichier_temp.write(header)
+
+# Obtention du nom de fichier temporaire
+nom_fichier_temp = fichier_temp.name
+
+# Options d'impression
+options_impression = {
+    "cpi": "18",
+    "lpi": "8",
+    # "page-left": "72",
+    # "page-top": "72",
+    "justify": "left"  # Justification à gauche / not weorking
+}
+
+# Impression du fichier temporaire
+impression = conn.printFile(epson, nom_fichier_temp, "impression_python.txt", options_impression)
+import os
+os.remove(nom_fichier_temp)
+
+printStartTime = time.time()
 for x in range(dots):
     # level = scale(data[x * 10, 0], -32768, 32767, 0, 844)
     # print("total average=", average(data, sampleStep * x, sampleStep))
     if getAverage:
         level = scale(average(data, sampleStep * x, sampleStep), -32768, 32767, 0, 844)
     else:
-        level = scale(data[sampleStep * x, 0], -32768, 32767, 0, 844)
+        level = scale(data[sampleStep * x + offset, 0], -32768, 32767, 0, 844)
     print("level =", level)
     generer_image(850, 6, 6, level, x)
-    show_image()
+    if showImage:
+        show_image()
     if printOrNot:
         job_id = conn.printFile(epson, dot_pict, "Titre du travail", options)
         print("Travail d'impression envoyé avec l'ID:", job_id)
@@ -127,3 +174,15 @@ for x in range(dots):
                 break
     print("Print {}/{}".format(currentDot, dots - 1))
     currentDot += 1
+printFinishTime = time.time()
+footer = "sampleStep={}\ndots/lines to print={}\noffset={}\nmethod={}\nprint length(S)={}\ntotal length={}m{}s\njob duration(S):{}\n\n".format(sampleStep, dots, offset, getMethod(), round(sampleStep * dots / 48000, 2), int(totalDurationM), int(totalDurationS), getPrintDuration())
+with tempfile.NamedTemporaryFile(mode='w', delete=False) as fichier_temp:
+    fichier_temp.write(footer)
+
+# Obtention du nom de fichier temporaire
+nom_fichier_temp = fichier_temp.name
+
+# Impression du fichier temporaire
+impression = conn.printFile(epson, nom_fichier_temp, "impression_python.txt", options_impression)
+import os
+os.remove(nom_fichier_temp)
